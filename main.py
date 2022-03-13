75d4d73970cc90796388e8743408c3e73759154a
triskofwhaleisland <67342722+triskofwhaleisland@users.noreply.github.com>
2022-03-13 19:22:29 -0400

    formatting

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import discord
 from discord.ext import commands
 from enum import Enum, auto
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import logging
 import yaml
 
-prefix = '.'
-version = "B is for probably Broken"
+prefix = "."
+version = "A is for Alpha"
 intents = discord.Intents.default()
 intents.messages = True
 intents.members = True
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), intents=intents)
-token = open('tokenfile').read()
+token = open("tokenfile").read()
 
 logging.basicConfig(level=logging.INFO)
 
@@ -31,11 +31,13 @@ class RecruitmentStatus(Enum):
 
 @dataclass
 class RecruitQueue:
-    queue: dict[discord.Member: RecruitmentStatus]
+    queue: dict[discord.Member : RecruitmentStatus]
     active_user: discord.Member | None = None
 
-    def update_user(self, user: discord.Member, status: RecruitmentStatus | None) -> None:
-        if not status:
+    def update_user(
+        self, user: discord.Member, status: RecruitmentStatus | None
+    ) -> None:
+        if status is None:
             if self.queue[user] == RecruitmentStatus.ACTIVE:
                 self.active_user = None
             del self.queue[user]
@@ -52,12 +54,14 @@ def load_recruiters_from_yaml(filename: str) -> dict:
             for (userID, statusName) in dict(yaml.safe_load(open(filename))).items()
         }
     except FileNotFoundError:
-        open(filename, 'w')
+        open(filename, "w")
         return {}
 
 
 def save_recruiters_to_yaml(queue: dict, filename: str) -> None:
-    yaml.safe_dump({user.id: status.name for (user, status) in queue.items()}, open(filename, 'w'))
+    yaml.safe_dump(
+        {user.id: status.name for (user, status) in queue.items()}, open(filename, "w")
+    )
 
 
 RQ = RecruitQueue({})
@@ -68,7 +72,7 @@ async def on_ready():
     global RQ
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("------")
-    recruiters = load_recruiters_from_yaml('recruiters.yaml')
+    recruiters = load_recruiters_from_yaml("recruiters.yaml")
     RQ = RecruitQueue(recruiters)
     await bot.get_channel(945513732115673192).send("I just restarted!")
 
@@ -77,26 +81,32 @@ async def on_ready():
 async def set_status(ctx: commands.Context, stat: str):
     try:
         if (
-                RecruitmentStatus[stat.upper()] == RecruitmentStatus.ACTIVE
-                and RecruitmentStatus.ACTIVE in RQ.queue.values()
+            RecruitmentStatus[stat.upper()] == RecruitmentStatus.ACTIVE
+            and RecruitmentStatus.ACTIVE in RQ.queue.values()
         ):
-            await ctx.channel.send(f"{RQ.active_user} is already actively recruiting. Ask them to leave first.")
+            await ctx.channel.send(
+                f"{RQ.active_user} is already actively recruiting. Ask them to leave first."
+            )
         else:
             RQ.update_user(ctx.author, RecruitmentStatus[stat.upper()])
             await ctx.channel.send(f"{ctx.author} is now {stat.lower()}.")
     except ValueError:
-        await ctx.channel.send(f"{stat.upper()} is not a valid status. "
-                               f"Valid statuses are: {', '.join([rs.name for rs in RecruitmentStatus])}.")
+        await ctx.channel.send(
+            f"{stat.upper()} is not a valid status. "
+            f"Valid statuses are: {', '.join([rs.name for rs in RecruitmentStatus])}."
+        )
     finally:
-        save_recruiters_to_yaml(RQ.queue, 'recruiters.yaml')
+        save_recruiters_to_yaml(RQ.queue, "recruiters.yaml")
 
 
 @bot.command()
-async def join(ctx: commands.Context): await set_status(ctx, 'active')
+async def join(ctx: commands.Context):
+    await set_status(ctx, "active")
 
 
 @bot.command()
-async def ready(ctx: commands.Context): await set_status(ctx, 'ready')
+async def ready(ctx: commands.Context):
+    await set_status(ctx, "ready")
 
 
 @bot.command()
@@ -111,13 +121,18 @@ async def leave(ctx: commands.Context):
 @bot.command()
 async def display(ctx: commands.Context):
     # await ctx.channel.send(RQ.queue)
-    await ctx.channel.send(embed=discord.Embed(
-        title="Current Recruiters",
-        description="Name:\t\t\t\tStatus\n" +
-                    (f"**{RQ.active_user}: ACTIVE**\n" if RQ.active_user else "") +
-                    "\n".join(f"{user}:\t\t\t\t{status.name}" for
-                              (user, status) in RQ.queue.items() if status != RecruitmentStatus.ACTIVE)
-    ).set_footer(text=f"Project BATON: {version}"))
+    await ctx.channel.send(
+        embed=discord.Embed(
+            title="Current Recruiters",
+            description="Name:\t\t\t\tStatus\n"
+            + (f"**{RQ.active_user}: ACTIVE**\n" if RQ.active_user is not None else "")
+            + "\n".join(
+                f"{user}:\t\t\t\t{status.name}"
+                for (user, status) in RQ.queue.items()
+                if status != RecruitmentStatus.ACTIVE
+            ),
+        ).set_footer(text=f"Project BATON: {version}")
+    )
 
 
 @bot.command()
@@ -127,16 +142,18 @@ async def user_from_id(ctx: commands.Context, id_: int):
 
 @bot.command()
 async def ping(ctx: commands.Context, role: RecruitmentStatus, *, msg: str = ""):
-    await ctx.channel.send(f"{ctx.author.mention} -> "
-                           f"{' '.join([user.mention for (user, status) in RQ.queue.items() if status == role])}"
-                           + (f": {msg}" if msg else "")
-                           if role in RQ.queue.values()
-                           else f"Literally nobody is {role.name.lower()} right now.")
+    await ctx.channel.send(
+        f"{ctx.author.mention} -> "
+        f"{' '.join([user.mention for (user, status) in RQ.queue.items() if status == role])}"
+        + (f": {msg}" if msg != "" else "")
+        if role in RQ.queue.values()
+        else f"Literally nobody is {role.name.lower()} right now."
+    )
 
 
 @bot.command()
 async def all_commands(ctx: commands.Context):
-    await ctx.channel.send(', '.join(bot.all_commands.keys()))
+    await ctx.channel.send(", ".join(bot.all_commands.keys()))
 
 
 bot.run(token)
