8f10582b67161bf00ee0ea3c99bca2144abde1a7
triskofwhaleisland <triskofwhaleisland@gmail.com>
2023-02-17 15:56:53 -0500

    formatting.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Self
+
 import discord
 from discord.ext import commands
 from enum import Enum, auto
@@ -5,24 +7,23 @@ from dataclasses import dataclass
 import logging
 import yaml
 
-prefix = "."
-version = "A is for Alpha"
-intents = discord.Intents.default()
-intents.messages = True
-intents.members = True
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), intents=intents)
-token = open("tokenfile").read()
+PREFIX = "."
+VERSION = "A is for Alpha"
+INTENTS = discord.Intents.all()
+# INTENTS.message_content = True
+# INTENTS.members = True
+BOT = commands.Bot(command_prefix=commands.when_mentioned_or(PREFIX), intents=INTENTS)
+TOKEN = open("tokenfile").read()
 
 logging.basicConfig(level=logging.INFO)
 
 
 class RecruitmentStatus(Enum):
     READY = auto()
-    # READY_CONDITIONAL = auto()
     ACTIVE = auto()
 
     @classmethod
-    async def convert(cls, ctx: commands.Context, role: str):
+    async def convert(cls, _ctx: commands.Context, role: str) -> Self | None:
         try:
             return cls[role.upper()]
         except ValueError:
@@ -31,7 +32,7 @@ class RecruitmentStatus(Enum):
 
 @dataclass
 class RecruitQueue:
-    queue: dict[discord.Member: RecruitmentStatus]
+    queue: dict[discord.Member : RecruitmentStatus]
     active_user: discord.Member | None = None
 
     def update_user(
@@ -50,11 +51,11 @@ class RecruitQueue:
 def load_recruiters_from_yaml(filename: str) -> dict:
     try:
         return {
-            bot.get_user(userID): RecruitmentStatus[statusName]
+            BOT.get_user(userID): RecruitmentStatus[statusName]
             for (userID, statusName) in dict(yaml.safe_load(open(filename))).items()
         }
     except FileNotFoundError:
-        open(filename, "w")
+        open(filename, "w").close()
         return {}
 
 
@@ -64,31 +65,32 @@ def save_recruiters_to_yaml(queue: dict, filename: str) -> None:
     )
 
 
-bot.RQ = RecruitQueue({})
+BOT.RQ = RecruitQueue({})
 
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+@BOT.event
+async def on_ready() -> None:
+    print(f"Logged in as {BOT.user} (ID: {BOT.user.id})")
     print("------")
     recruiters = load_recruiters_from_yaml("recruiters.yaml")
-    bot.RQ = RecruitQueue(recruiters)
-    await bot.get_channel(945513732115673192).send("I just restarted!")
+    BOT.RQ = RecruitQueue(recruiters)
+    await BOT.get_channel(945513732115673192).send("I just restarted!")
 
 
-@bot.command()
-async def set_status(ctx: commands.Context, stat: str):
+@BOT.command()
+async def set_status(ctx: commands.Context, stat: str) -> None:
     try:
         if (
             RecruitmentStatus[stat.upper()] == RecruitmentStatus.ACTIVE
-            and RecruitmentStatus.ACTIVE in bot.RQ.queue.values()
-            and bot.RQ.active_user is not None
+            and RecruitmentStatus.ACTIVE in BOT.RQ.queue.values()
+            and BOT.RQ.active_user is not None
         ):
             await ctx.channel.send(
-                f"{bot.RQ.active_user} is already actively recruiting. Ask them to leave first."
+                f"{BOT.RQ.active_user} is already actively recruiting. "
+                f"Ask them to leave first."
             )
         else:
-            bot.RQ.update_user(ctx.author, RecruitmentStatus[stat.upper()])
+            BOT.RQ.update_user(ctx.author, RecruitmentStatus[stat.upper()])
             await ctx.channel.send(f"{ctx.author} is now {stat.lower()}.")
     except ValueError:
         await ctx.channel.send(
@@ -96,81 +98,95 @@ async def set_status(ctx: commands.Context, stat: str):
             f"Valid statuses are: {', '.join([rs.name for rs in RecruitmentStatus])}."
         )
     finally:
-        save_recruiters_to_yaml(bot.RQ.queue, "recruiters.yaml")
+        save_recruiters_to_yaml(BOT.RQ.queue, "recruiters.yaml")
 
 
-@bot.command()
-async def join(ctx: commands.Context):
+@BOT.command()
+async def join(ctx: commands.Context) -> None:
     await set_status(ctx, "active")
 
 
-@bot.command()
-async def ready(ctx: commands.Context):
+@BOT.command()
+async def ready(ctx: commands.Context) -> None:
     await set_status(ctx, "ready")
 
 
-@bot.command()
-async def leave(ctx: commands.Context):
-    if ctx.author in bot.RQ.queue.keys():
-        bot.RQ.update_user(ctx.author, None)
+@BOT.command()
+async def leave(ctx: commands.Context) -> None:
+    if ctx.author in BOT.RQ.queue.keys():
+        BOT.RQ.update_user(ctx.author, None)
         await ctx.channel.send(f"{ctx.author} has left the queue.")
     else:
         await ctx.channel.send("You can't leave something you aren't a part of!")
 
 
-@bot.command()
-async def display(ctx: commands.Context):
+@BOT.command()
+async def display(ctx: commands.Context) -> None:
     # await ctx.channel.send(RQ.queue)
     await ctx.channel.send(
         embed=discord.Embed(
             title="Current Recruiters",
-            description="Name:\t\t\t\tStatus\n"
-                        + (f"**{bot.RQ.active_user}: ACTIVE**\n" if bot.RQ.active_user is not None else "")
-                        + "\n".join(
-                f"{user}:\t\t\t\t{status.name}"
-                for (user, status) in bot.RQ.queue.items()
-                if status != RecruitmentStatus.ACTIVE
+            description=(
+                "Name:\t\t\t\tStatus\n"
+                + (
+                    f"**{BOT.RQ.active_user}: ACTIVE**\n"
+                    if BOT.RQ.active_user is not None
+                    else ""
+                )
+                + "\n".join(
+                    f"{user}:\t\t\t\t{status.name}"
+                    for (user, status) in BOT.RQ.queue.items()
+                    if status != RecruitmentStatus.ACTIVE
+                )
             ),
-        ).set_footer(text=f"Project BATON: {version}")
+        ).set_footer(text=f"Project BATON: {VERSION}")
     )
 
 
-@bot.command()
-async def user_from_id(ctx: commands.Context, id_: int):
-    await ctx.channel.send(bot.get_user(id_))
+@BOT.command()
+async def user_from_id(ctx: commands.Context, id_: int) -> None:
+    await ctx.channel.send(BOT.get_user(id_))
 
 
-@bot.command()
-async def ping(ctx: commands.Context, role: RecruitmentStatus, *, msg: str = ""):
+@BOT.command()
+async def ping(
+    ctx: commands.Context, role: RecruitmentStatus, *, msg: str = ""
+) -> None:
     await ctx.channel.send(
         f"{ctx.author.mention} -> "
-        f"{' '.join([user.mention for (user, status) in bot.RQ.queue.items() if status == role])}"
-        + (f": {msg}" if msg != "" else "")
-        if role in bot.RQ.queue.values()
-        else f"Literally nobody is {role.name.lower()} right now."
+        f"{' '.join([user.mention for (user, status) in BOT.RQ.queue.items() if status == role])}"
+        + (
+            f": {msg}"
+            if msg != "" and role in BOT.RQ.queue.values()
+            else f"Literally nobody is {role.name.lower()} right now."
+        )
     )
 
 
-@bot.command()
-async def all_commands(ctx: commands.Context):
-    await ctx.channel.send(", ".join(bot.all_commands.keys()))
+@BOT.command()
+async def all_commands(ctx: commands.Context) -> None:
+    await ctx.channel.send(", ".join(BOT.all_commands.keys()))
 
 
-@bot.command()
+@BOT.command()
 @commands.has_permissions(administrator=True)
-async def remove_from_queue(ctx: commands.Context, user: discord.Member):
+async def remove_from_queue(ctx: commands.Context, user: discord.Member) -> None:
     try:
-        bot.RQ.update_user(user, None)
+        BOT.RQ.update_user(user, None)
     except KeyError:
         await ctx.channel.send(f"{user} is not in the queue. L + clueless :P")
 
 
 @remove_from_queue.error
-async def remove_from_queue_error(error, ctx):
+async def remove_from_queue_error(
+    error: commands.CommandError, ctx: commands.Context
+) -> None:
     if isinstance(error, commands.CheckFailure):
         await ctx.channel.send("Yikes sweaty, check your (Discord) privilege(s).")
     else:
-        await ctx.channel.send(f"There was an error removing that user from the queue: {error}")
+        await ctx.channel.send(
+            f"There was an error removing that user from the queue: {error}"
+        )
 
 
-bot.run(token)
+BOT.run(TOKEN)
